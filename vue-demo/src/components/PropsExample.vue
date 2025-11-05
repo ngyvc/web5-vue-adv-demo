3a6bd26d25735050303528f5aff7de32d2029298
Peter Ng <ngy@vaniercollege.qc.ca>
2025-11-05 09:22:04 -0500

    add props

---
+++ vue-demo/src/components/PropsExample.vue
@@ -0,0 +1,26 @@
+<script setup>
+const props = defineProps({
+  title: {
+    type: String,
+    default: 'Default Title',
+    // required: true
+  },
+  author: {
+    type: String,
+    default: 'Unknown Author',
+    // required: true
+  },
+  content: {
+    type: String,
+    default: 'No content available.',
+    // required: true
+  },
+})
+</script>
+<template>
+  <section class="props-example">
+    <h2>{{ props.title }}</h2>
+    <p>Author: {{ props.author }}</p>
+    <p>{{ props.content }}</p>
+  </section>
+</template>
