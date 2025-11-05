b8d8fbdb88926a29549bd65026cf040595c314ea
Peter Ng <ngy@vaniercollege.qc.ca>
2025-11-04 21:46:25 -0500

    update filters

--- vue-demo/src/views/WpLoadView.vue
+++ vue-demo/src/views/WpLoadView.vue
@@ -7,7 +7,10 @@ import WpLoad from '../components/WpLoad.vue';
     <WpLoad />
     <WpLoad title="projects" url="https://ngy.582mi.com/headless/wp-json/wp/v2/projects" />
     <WpLoad title="media types" url="https://ngy.582mi.com/headless/wp-json/wp/v2/media_types" />
+    <!-- <WpLoad title="media types" url="https://ngy.582mi.com/headless/wp-json/wp/v2/media_types?orderby=id" />
+    <WpLoad title="media types" url="https://ngy.582mi.com/headless/wp-json/wp/v2/media_types?orderby=name&order=desc" /> -->
     <!-- <WpLoad title="media types 12" url="https://ngy.582mi.com/headless/wp-json/wp/v2/projects?media_types=12" />
+    <WpLoad title="media types 12" url="https://ngy.582mi.com/headless/wp-json/wp/v2/projects?media_types=14&orderby=id&order=asc" />
     <WpLoad title="media types 13" url="https://ngy.582mi.com/headless/wp-json/wp/v2/projects?media_types=13" />
     <WpLoad title="media types 14" url="https://ngy.582mi.com/headless/wp-json/wp/v2/projects?media_types=14" />
     <WpLoad title="media types 15" url="https://ngy.582mi.com/headless/wp-json/wp/v2/projects?media_types=15" />
