45720d55ba3e8696d4e5031692f39efbdc327795
Peter Ng <ngy@vaniercollege.qc.ca>
2025-11-05 09:45:54 -0500

    nested route

--- vue-demo/src/router/index.js
+++ vue-demo/src/router/index.js
@@ -48,6 +48,11 @@ const router = createRouter({
       name: 'ActionTrigger',
       component: () => import('../views/ActionTriggerView.vue'),
     },
+    {
+      path: '/nested/:id',
+      name: 'Nested',
+      component: () => import('../views/NestedView.vue'),
+    }
   ],
 })
 
---
+++ vue-demo/src/views/NestedView.vue
@@ -0,0 +1,12 @@
+<script setup>
+</script>
+<template>
+  <section>
+    Data from route: {{ $route.params.id }}
+  </section>
+</template>
+<style lang="scss" scoped>
+section {
+  margin-top: 4em;
+}
+</style>
