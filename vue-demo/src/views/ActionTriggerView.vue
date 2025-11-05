27e05702011c9fbc04c9a00a13d6451bbc6a20d9
Peter Ng <ngy@vaniercollege.qc.ca>
2025-11-05 09:22:27 -0500

    add action

---
+++ vue-demo/src/views/ActionTriggerView.vue
@@ -0,0 +1,20 @@
+<script setup>
+import ActionTrigger from '../components/ActionTrigger.vue'
+</script>
+
+<template>
+  <section class="action-trigger-view">
+    <h1>Action Trigger View</h1>
+    <ActionTrigger />
+  </section>
+</template>
+
+<style lang="scss" scoped>
+.action-trigger-view {
+  margin-top: 4rem;
+
+  h1 {
+    text-transform: capitalize;
+  }
+}
+</style>
